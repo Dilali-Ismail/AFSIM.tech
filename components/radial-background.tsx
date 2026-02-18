34cf3424e47c0fcd67ec87fc00cfc8eaf1750488
Dilaly <ismaildilali@gmail.com>
2026-02-18 22:15:38 +0100

    change the background 2

--- components/radial-background.tsx
+++ components/radial-background.tsx
@@ -5,12 +5,12 @@ import React from 'react';
 export function RadialBackground() {
     // Generate a few lines with slightly different timings
     const lines = [
-        { d: "M-100,50 Q250,150 600,50 T1300,50", dur: "15s", delay: "0s", top: "10%" },
-        { d: "M-100,60 Q300,10 700,60 T1400,60", dur: "18s", delay: "-2s", top: "25%" },
-        { d: "M-100,50 Q350,100 800,50 T1500,50", dur: "20s", delay: "-5s", top: "40%" },
-        { d: "M-100,70 Q400,20 900,70 T1600,70", dur: "22s", delay: "-1s", top: "55%" },
-        { d: "M-100,40 Q450,120 1000,40 T1700,40", dur: "16s", delay: "-3s", top: "70%" },
-        { d: "M-100,80 Q500,30 1100,80 T1800,80", dur: "25s", delay: "-8s", top: "85%" },
+        { d: "M-100,50 Q250,150 600,50 T1300,50", dur: "15s", begin: "0s", top: "10%" },
+        { d: "M-100,60 Q300,10 700,60 T1400,60", dur: "18s", begin: "-2s", top: "25%" },
+        { d: "M-100,50 Q350,100 800,50 T1500,50", dur: "20s", begin: "-5s", top: "40%" },
+        { d: "M-100,70 Q400,20 900,70 T1600,70", dur: "22s", begin: "-1s", top: "55%" },
+        { d: "M-100,40 Q450,120 1000,40 T1700,40", dur: "16s", begin: "-3s", top: "70%" },
+        { d: "M-100,80 Q500,30 1100,80 T1800,80", dur: "25s", begin: "-8s", top: "85%" },
     ];
 
     return (
@@ -46,7 +46,7 @@ export function RadialBackground() {
                             <animate
                                 attributeName="d"
                                 dur={line.dur}
-                                delay={line.delay}
+                                begin={line.begin}
                                 repeatCount="indefinite"
                                 values={`
                                     ${line.d};
